29cebcc02974d7fe56cd7d4b481d4539475a9305
sabreeshbhatt <sabreesh@a483e78e93b7.ant.amazon.com>
2020-06-21 16:36:17 +0530

    added displayboard function

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"strings"
 )
 
+func DisplayBoard(board [][]string) { //DisplayBoard...Function to display TicTacToe Board
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
 func main() {
 	// creating a tictactoe board
 	board := [][]string{
@@ -14,7 +20,5 @@ func main() {
 		[]string{"-", "-", "-", "-", "-"},
 		[]string{" ", "|", " ", "|", " "},
 	}
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
-	}
+	DisplayBoard(board)
 }
